1c36fe109ce8109d3154aa4c0a0dcb8f48e93733
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2024-10-12 16:48:16 -0300

    config migrations

---
+++ cmd/migrate/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:1234@localhost:5432/doti_db?sslmode=disable")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://src/infra/database/store/pgstore/migrations/", // migrations path
+		"postgres", driver)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	} else {
+		log.Println("Migrations applied successfully")
+	}
+}
