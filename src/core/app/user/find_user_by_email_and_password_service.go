90b76c9e64a0dbdf06e5823d1f59cbd310aa5704
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2025-05-21 23:33:43 -0300

    SRP: find user by email and password

--- src/core/app/user/find_user_by_email_and_password_service.go
+++ src/core/app/user/find_user_by_email_and_password_service.go
@@ -4,11 +4,28 @@ import (
 	"context"
 
 	"github.com/Giovani-Coelho/Doti-API/src/infra/persistence/db/sqlc"
+	"github.com/Giovani-Coelho/Doti-API/src/infra/persistence/repository"
 	"github.com/Giovani-Coelho/Doti-API/src/pkg/handlers/encrypt"
 	rest_err "github.com/Giovani-Coelho/Doti-API/src/pkg/handlers/http"
 )
 
-func (us *UserServices) findUserByEmailAndPassword(
+type FindUserByEmailAndPasswordService struct {
+	UserRepository repository.IUserRepository
+}
+
+type IFindUserByEmailAndPasswordService interface {
+	Execute(ctx context.Context, email string, password string) (sqlc.User, error)
+}
+
+func NewFindUserByEmailAndPasswordService(
+	userRepository repository.IUserRepository,
+) IFindUserByEmailAndPasswordService {
+	return &FindUserByEmailAndPasswordService{
+		UserRepository: userRepository,
+	}
+}
+
+func (us *FindUserByEmailAndPasswordService) Execute(
 	ctx context.Context,
 	email string,
 	password string,
--- src/infra/http/server/routes.go
+++ src/infra/http/server/routes.go
@@ -3,28 +3,10 @@ package server
 import (
 	"database/sql"
 	"net/http"
-
-	authServices "github.com/Giovani-Coelho/Doti-API/src/core/app/auth"
-	userServices "github.com/Giovani-Coelho/Doti-API/src/core/app/user"
-	userController "github.com/Giovani-Coelho/Doti-API/src/infra/http/controller/user"
-	"github.com/Giovani-Coelho/Doti-API/src/infra/persistence/repository"
 )
 
 func Routes(DB *sql.DB) *http.ServeMux {
 	router := http.NewServeMux()
-
-	// Repository
-	userRepository := repository.NewUserRepository(DB)
-
-	// UseCase
-	userServices := userServices.NewUserServices(userRepository)
-	authServices := authServices.NewAuthServices(userRepository)
-
-	// Controller
-	userController := userController.NewUserControllers(userServices, authServices)
-
-	router.HandleFunc("/account", userController.CreateUser)
-	router.HandleFunc("/login", userController.LoginUser)
 
 	return router
 }
