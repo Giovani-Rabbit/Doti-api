ca7df32f9ba8ebd4606b995dcefc791a6e6b09dd
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2025-07-14 17:20:59 -0300

    feat: module name table column

--- src/infra/persistence/db/migrations/20250714200216_module.up.sql
+++ src/infra/persistence/db/migrations/20250714200216_module.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS modules (
     id         UUID                     NOT NULL    PRIMARY KEY,
     user_id    UUID                     NOT NULL,
+    name       VARCHAR(255)             NOT NULL,
     is_open    BOOLEAN                              DEFAULT false,
     icon       VARCHAR(255)             NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL    DEFAULT now(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL    DEFAULT now()
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL    DEFAULT now(),
 
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 )
