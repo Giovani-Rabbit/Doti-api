7a6c4f26b786354137da64e1b411a1ee48bbb8fd
Giovani-Rabbit <giovanicoelho2004@hotmail.com>
2025-09-13 19:49:42 -0300

    test: module id validation

--- internal/core/app/task/create_task_usecase_test.go
+++ internal/core/app/task/create_task_usecase_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	taskcase "github.com/Giovani-Coelho/Doti-API/internal/core/app/task"
+	moduledomain "github.com/Giovani-Coelho/Doti-API/internal/core/domain/module"
 	taskdomain "github.com/Giovani-Coelho/Doti-API/internal/core/domain/task"
 	resp "github.com/Giovani-Coelho/Doti-API/internal/infra/http/responder"
 	mock_repository "github.com/Giovani-Coelho/Doti-API/internal/infra/persistence/repository/mocks"
@@ -51,6 +52,28 @@ func TestCreateTaskUseCase(t *testing.T) {
 		if respErr.Status != taskdomain.SttInvalidFields {
 			t.Errorf("Expected %v error, got: %v",
 				taskdomain.SttInvalidFields,
+				respErr.Status,
+			)
+		}
+	})
+
+	t.Run("Should fail if module does not exists", func(t *testing.T) {
+		task := taskdomain.New(1, "task", 2)
+
+		moduleRepository.EXPECT().
+			CheckExistsById(gomock.Any(), task.ModuleId()).
+			Return(false, nil)
+
+		_, err := createTask.Execute(ctx, task)
+		if err == nil {
+			t.Errorf("Expected error, got: %v", err)
+		}
+
+		respErr := resp.AsRestErr(err)
+
+		if respErr.Status != moduledomain.SttCouldNotFindModuleByID {
+			t.Errorf("Expected status %v, got: %v",
+				moduledomain.SttCouldNotFindModuleByID,
 				respErr.Status,
 			)
 		}
